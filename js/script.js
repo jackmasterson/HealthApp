10917d67a00495178b7bc1462842e8806b0b250d
JackMasterson <jackmasterson5@gmail.com>
2016-05-24 16:39:11 -0400

    addsList
    when you search, the search div populates with the searched info

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+var model = {
+	foodInfo: []
+};
+
 var nutr = {
 	query: function(){
 		var search = document.getElementsByClassName("searchBar");
@@ -6,15 +10,12 @@ var nutr = {
 		var calMax = document.getElementsByClassName("maxBar");
 		var self = this;
 		$(".searchButton").click(function(event){
-		    
 		      self.searched = $(search[0]).val();
 		      self.calMin = $(calMin[0]).val();
 		      self.calMax = $(calMax[0]).val();
 		      self.init();
 		   
-		});
-
-		
+		});	
 	},
 
 	init: function() {
@@ -49,19 +50,35 @@ var nutr = {
 			for(var i=0; i<response.hits.length; i++){
 				//console.log(response.hits[i].fields);
 				var fields = response.hits[i].fields;
-			//	console.log(fields);
+				console.log(fields);
 				var item = fields.item_name;
 				var brand = fields.brand_name;
 				var cal = fields.nf_calories;
 				var serving = fields.nf_serving_size_qty;
+				var unit = fields.nf_serving_size_unit;
 				var fat = fields.nf_total_fat;
 			//	console.log(item, brand, cal, serving, fat);
-
-				//console.log(fields.item_name);
+				model.foodInfo = [];
+				model.foodInfo.push({
+					"item": item,
+					"brand": brand,
+					"cal": cal,
+					"serving": serving + " " + unit,
+					"fat": fat
+				});
+			//	console.log(model.foodInfo);
+				var itemHead = '<h4><a onClick="add.init()">'+item+'-'+brand+'</h4>'+
+					'<h4>'+cal+' '+serving+' '+unit+'</h4></a>';
+				$('.search-fill').append(itemHead);
 			}
 		})
 
+	}
+};
 
+var add = {
+	init: function() {
+		console.log('hey');
 	}
 };
 
