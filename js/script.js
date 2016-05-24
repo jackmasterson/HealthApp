6d7fe225336b50ffd002cba0a55c7acbf2d09591
JackMasterson <jackmasterson5@gmail.com>
2016-05-24 16:10:29 -0400

    caloriesSearch
    can search with calorie minimum and maximum

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@
 var nutr = {
 	query: function(){
 		var search = document.getElementsByClassName("searchBar");
+		var calMin = document.getElementsByClassName("minBar");
+		var calMax = document.getElementsByClassName("maxBar");
 		var self = this;
-		$(".searchBar").keyup(function(event){
-		    if(event.keyCode == 13){
+		$(".searchButton").click(function(event){
+		    
 		      self.searched = $(search[0]).val();
+		      self.calMin = $(calMin[0]).val();
+		      self.calMax = $(calMax[0]).val();
 		      self.init();
-		    }
+		   
 		});
 
 		
@@ -17,11 +21,20 @@ var nutr = {
 		console.log(this.query);
 		var apiID = "20ca6dec";
 		var apiKey = "2926b7be1f28ff2f6cb361a23678110c";
-		this.nutrUrl = "https://api.nutritionix.com/v1_1/search/"+
+		this.nutrUrl = /*"https://api.nutritionix.com/v1_1/search/"+
 			this.searched +
 			"?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories"+
-			"%2Cnf_total_fat&appId="+ apiID+"&appKey="+apiKey;
-
+			"%2Cnf_total_fat&appId="+ apiID+"&appKey="+apiKey;*/
+			"https://api.nutritionix.com/v1_1/search/"+
+				this.searched+
+				"?results="+"0%3A20&cal_min="+
+				this.calMin+
+				"&cal_max="+
+				this.calMax+
+				"&fields=*&appId="+
+				apiID+
+				"&appKey="+
+				apiKey;
 		this.render();
 	},
 
@@ -33,7 +46,22 @@ var nutr = {
 		})
 		.done(function(response){
 			console.log(response);
+			for(var i=0; i<response.hits.length; i++){
+				//console.log(response.hits[i].fields);
+				var fields = response.hits[i].fields;
+			//	console.log(fields);
+				var item = fields.item_name;
+				var brand = fields.brand_name;
+				var cal = fields.nf_calories;
+				var serving = fields.nf_serving_size_qty;
+				var fat = fields.nf_total_fat;
+			//	console.log(item, brand, cal, serving, fat);
+
+				//console.log(fields.item_name);
+			}
 		})
+
+
 	}
 };
 
