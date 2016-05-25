4f0e5b453fc46dddd8105f03934160044c247328
JackMasterson <jackmasterson5@gmail.com>
2016-05-25 11:25:55 -0400

    workingWithBackbone
    trying to make the app use backbone correctly - first run

--- js/script.js
+++ js/script.js
@@ -1,87 +1,143 @@
 
-var model = {
-	foodInfo: []
-};
+(function($){
+	var Item = Backbone.Model.extend({
+		foodInfo: [],
+		defaults: {
+			part1: 'hello',
+			part2: 'world',
+			item: 'jack'
+		}
+	});
 
-var nutr = {
-	query: function(){
-		var search = document.getElementsByClassName("searchBar");
-		var calMin = document.getElementsByClassName("minBar");
-		var calMax = document.getElementsByClassName("maxBar");
-		var self = this;
-		$(".searchButton").click(function(event){
-		      self.searched = $(search[0]).val();
-		      self.calMin = $(calMin[0]).val();
-		      self.calMax = $(calMax[0]).val();
-		      $(".search-fill").slideDown();
-		      self.init();
+	var List = Backbone.Collection.extend({
+		model: Item
+	});
 
-		   
-		});	
-	},
+	var ItemView = Backbone.View.extend({
+		tagName: 'li', 
+		initialize: function() {
+			_.bindAll(this, 'render');
+		},
+		render: function() {
+			$(this.el).html('<span>'+this.model.get('item')+'</span>');
+			return this;
+		}
+	})
 
-	init: function() {
-		console.log(this.query);
-		var apiID = "20ca6dec";
-		var apiKey = "2926b7be1f28ff2f6cb361a23678110c";
-		this.nutrUrl = /*"https://api.nutritionix.com/v1_1/search/"+
-			this.searched +
-			"?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories"+
-			"%2Cnf_total_fat&appId="+ apiID+"&appKey="+apiKey;*/
-			"https://api.nutritionix.com/v1_1/search/"+
-				this.searched+
-				"?results="+"0%3A20&cal_min="+
-				this.calMin+
-				"&cal_max="+
-				this.calMax+
-				"&fields=*&appId="+
-				apiID+
-				"&appKey="+
-				apiKey;
+	var ListView = Backbone.View.extend({
+		el: $('body'),
+		events: {
+			'click button.searchButton': 'query',
+			'click a.foodAdd': 'addTo'
+		},
+
+		initialize: function() {
+		_.bindAll(this, 'render', 'query', 'addTo');
+		
+		this.collection = new List();
+		this.collection.bind('add', this.query, this.addTo);
+
+	//	this.counter = 0;
 		this.render();
-	},
 
-	render: function() {
+		},
 
-		$.ajax({
-			url: this.nutrUrl,
-			dataType: 'json',
-		})
-		.done(function(response){
-			console.log(response);
-			for(var i=0; i<response.hits.length; i++){
-				//console.log(response.hits[i].fields);
-				var fields = response.hits[i].fields;
-				console.log(fields);
-				var item = fields.item_name;
-				var brand = fields.brand_name;
-				var cal = fields.nf_calories;
-				var serving = fields.nf_serving_size_qty;
-				var unit = fields.nf_serving_size_unit;
-				var fat = fields.nf_total_fat;
-			//	console.log(item, brand, cal, serving, fat);
-				model.foodInfo = [];
-				model.foodInfo.push({
-					"item": item,
-					"brand": brand,
-					"cal": cal,
-					"serving": serving + " " + unit,
-					"fat": fat
-				});
-			//	console.log(model.foodInfo);
-				var itemHead = '<h4><a onClick="add.init()">'+item+'-'+brand+'</h4>'+
-					'<h4>'+cal+' calories, '+serving+' '+unit+'</h4></a>';
-				$('.search-fill').append(itemHead);
-			}
-		})
+		render: function() {
+			var self = this;
 
-	}
-};
+			$(this.el).prepend("<button class='searchButton'>Click to Search!</button>");
+			//$(this.el).append("<ul></ul>");
 
-var add = {
-	init: function() {
-		console.log('hey');
-	}
-};
+		},
 
-nutr.query()
+		query: function(){
+			var stuff= new Item();
+			var search = document.getElementsByClassName("searchBar");
+			var calMin = document.getElementsByClassName("minBar");
+			var calMax = document.getElementsByClassName("maxBar");
+			var self = this;
+	        self.searched = $(search[0]).val();
+	        self.calMin = $(calMin[0]).val();
+	        self.calMax = $(calMax[0]).val();
+	        $(".search-fill").slideDown();
+	        self.nutrInit();  
+		},		
+
+		nutrInit: function() {
+			console.log(this.query);
+			var apiID = "20ca6dec";
+			var apiKey = "2926b7be1f28ff2f6cb361a23678110c";
+			this.nutrUrl =	
+			"https://api.nutritionix.com/v1_1/search/"+
+					this.searched+
+					"?results="+"0%3A20&cal_min="+
+					this.calMin+
+					"&cal_max="+
+					this.calMax+
+					"&fields=*&appId="+
+					apiID+
+					"&appKey="+
+					apiKey;
+
+			this.nutrRender();
+		},
+
+		nutrRender: function() {
+
+			$.ajax({
+				url: this.nutrUrl,
+				dataType: 'json',
+			})
+			.done(function(response){
+
+				for(var i=0; i<response.hits.length; i++){
+					var fields = response.hits[i].fields;
+					var item = fields.item_name;
+					var brand = fields.brand_name;
+					var cal = fields.nf_calories;
+					var serving = fields.nf_serving_size_qty;
+					var unit = fields.nf_serving_size_unit;
+					var fat = fields.nf_total_fat;
+					Item.foodInfo = [];
+					Item.foodInfo.push({
+						"item": item,
+						"brand": brand,
+						"cal": cal,
+						"serving": serving + " " + unit,
+						"fat": fat
+					});
+
+					var itemView = new ItemView({
+						model: item
+					});
+					$('ul', this.el).append(itemView.render().el);
+//make this a template.////////////
+					var itemHead = 
+						'<div>'+
+							'<h2 class="food-head">' +
+								'<a class="foodAdd">'
+									+item+'-'+brand+
+								'</a>'+
+							'</h2>'+
+							'<h4 class="food-sub-head">'
+								+cal+' calories, '+serving+' '+unit+
+							'</h4>'+
+						'</div><br>';
+
+					$('.search-fill').append(itemHead);
+				}
+			})
+		},
+
+		addTo: function() {
+			var that = this;
+			console.log('hey');
+			console.log(this.$('.foodAdd')[0]);
+		}
+	});
+
+	var listView = new ListView();
+
+}) (jQuery);
+
+
